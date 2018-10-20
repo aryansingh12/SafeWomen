28ebf93256fb7c7bbaf2656c87b43c600d36aaef
aryan <aspiringaryan@gmail.com>
2018-10-20 02:32:57 -0400

    Shake to call, user location

--- app/src/main/java/com/hackumass/med/redhacks/EditProfile.java
+++ app/src/main/java/com/hackumass/med/redhacks/EditProfile.java
@@ -9,5 +9,7 @@ public class EditProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+        getSupportActionBar().setTitle("Edit your profile");
+
     }
 }
--- app/src/main/java/com/hackumass/med/redhacks/MainActivity.java
+++ app/src/main/java/com/hackumass/med/redhacks/MainActivity.java
@@ -3,14 +3,26 @@ package com.hackumass.med.redhacks;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Toast;
 
 import com.github.nisrulz.sensey.Sensey;
 import com.github.nisrulz.sensey.ShakeDetector;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -18,15 +30,59 @@ public class MainActivity extends AppCompatActivity {
     private ShakeDetector shakeDetector;
     float threshold = 0f;
     long timeBeforeDeclaringShakeStopped = 0;
+    double lat;
+    double lng;
+    FusedLocationProviderClient client;
+    LocationCallback callback;
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        LocationRequest request = new LocationRequest();
+        request.setInterval(5000);
+        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        request.setFastestInterval(3000);
+
+        callback = new LocationCallback(){
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if(locationResult != null){
+                    List<Location> locations = locationResult.getLocations();
+                    for(Location location: locations){
+//                        Log.d("MainActivity","Location: Lat-" + location.getLatitude() + " Long:-" + location.getLongitude() );
+                    }
+                }
+                super.onLocationResult(locationResult);
+            }
+        };
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        client.requestLocationUpdates(request,callback, null);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        client.removeLocationUpdates(callback);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Sensey.getInstance().init(this);
-        ShakeDetector.ShakeListener shakeListener=new ShakeDetector.ShakeListener() {
-            @Override public void onShakeDetected() {
+        ShakeDetector.ShakeListener shakeListener = new ShakeDetector.ShakeListener() {
+            @Override
+            public void onShakeDetected() {
                 // Shake detected, do something
                 if(ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED){
                     call("999999999");
@@ -34,14 +90,59 @@ public class MainActivity extends AppCompatActivity {
                     String[] permissions = {Manifest.permission.CALL_PHONE};
                     ActivityCompat.requestPermissions(MainActivity.this,permissions,1);
                 }
+//                shareText();
             }
-            @Override public void onShakeStopped() {
+
+            @Override
+            public void onShakeStopped() {
                 // Shake stopped, do something
             }
         };
         Sensey.getInstance().startShakeDetection(shakeListener);
 
         //Sensey.getInstance().startShakeDetection(threshold,timeBeforeDeclaringShakeStopped,shakeListener);
+
+
+        client = LocationServices.getFusedLocationProviderClient(this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        client.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+//                    textView.setText("Lat:" + location.getLatitude() + " Long:" + location.getLongitude());
+                    lat = location.getLatitude();
+                    lng = location.getLongitude();
+                    Toast.makeText(MainActivity.this,lat + "" + lng + "",Toast.LENGTH_LONG).show();
+                }
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(MainActivity.this,lat + lng + "",Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+    }
+
+
+    public void dial(){
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED){
+            call("999999999");
+        }else {
+            String[] permissions = {Manifest.permission.CALL_PHONE};
+            ActivityCompat.requestPermissions(this,permissions,1);
+        }
     }
     public void call(String phone){
         Intent intent = new Intent();
@@ -51,9 +152,18 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-//    @Override
-//    protected void onDestroy() {
-//        super.onDestroy();
-//        Sensey.getInstance().stop();
-//    }
+    public void shareText(){
+
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT,"abcd");
+        startActivity(intent);
+
+    }
+
+    public void profile(View view){
+    Intent i = new Intent(this,EditProfile.class);
+    startActivity(i);
+    }
 }
